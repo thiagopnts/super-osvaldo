a2ac4485db4052d738466ebbf66ceddbed0ccdae
thiagopnts <thiagopnts@gmail.com>
2014-06-05 00:04:07 -0300

    plot the entire atlas

--- SuperOsvaldo/GameScene.swift
+++ SuperOsvaldo/GameScene.swift
@@ -10,13 +10,35 @@ import SpriteKit
 
 class GameScene: SKScene {
     override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, Osvaldo!";
-        myLabel.fontSize = 65;
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
+
+        let texture = SKTexture(imageNamed:"osvaldo")
+        let sprite = SKSpriteNode(texture: texture)
+
+        let firstFrame = SKTexture(rect:CGRectMake(0.0, 0.0, 0.5, 0.5), inTexture: texture)
+
+        sprite.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame));
+
+        self.addChild(sprite)
+    }
+    
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        /* Called when a touch begins */
         
-        self.addChild(myLabel)
+        for touch: AnyObject in touches {
+            let location = touch.locationInNode(self)
+            
+            let sprite = SKSpriteNode(imageNamed:"osvaldo")
+            
+            sprite.xScale = 0.5
+            sprite.yScale = 0.5
+            sprite.position = location
+            
+            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
+            
+            sprite.runAction(SKAction.repeatActionForever(action))
+            
+            self.addChild(sprite)
+        }
     }
    
     override func update(currentTime: CFTimeInterval) {
